62ee6145502045a1f4d238d437913bdc2fe7258b
Sachindra Pandey <sachindra.pandey@stigasoft.com>
2026-03-27 15:02:54 +0530

    igniting our app completed

--- 01_Inception/App.js
+++ 01_Inception/App.js
@@ -1,16 +1,16 @@
-// const element1 = React.createElement("h1", { id: "banner" }, "Header");
-// const element2 = React.createElement("input", { style: { color: "red" } });
-// const element3 = React.createElement("h2", {}, "Footer");
-// const div = React.createElement("div", {}, element1, element2, element3);
+import React from "react";
+
+import ReactDOM from "react-dom/client";
 
 const container1 = React.createElement(
   "div",
   { key: "1" },
 
-  React.createElement("h1", {}, "jCodeCamp"),
+  React.createElement("h1", {}, ["🚀", "  jCodeCamp"]),
   React.createElement(
     "p",
     { key: "2" },
+    "⭐",
     "freeCodeCamp is a non-profit educational organization that consists of an interactive learning web platform, an online community forum, chat rooms, online publications and local organizations that intend to make learning software development & computer programming accessible to anyone.",
   ),
   React.createElement("h2", {}, "Language We Covered"),
